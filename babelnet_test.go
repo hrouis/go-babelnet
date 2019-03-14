12d3c6e0f336828ba9ace23078332f9607fcd290
haythem_rouis <haythem.rouis@gmail.com>
2019-03-14 22:07:14 +0100

    update test

--- babelnet_test.go
+++ babelnet_test.go
@@ -1,28 +1,46 @@
 package babelnet
 
 import (
-	"fmt"
+	"gotest.tools/assert"
 	"testing"
 )
 
+var client = NewClient("https://babelnet.io", "40590341-692c-4b9d-beb7-1901e4e6e985")
+
 func TestClient_GetVersion(t *testing.T) {
-	client := NewClient("https://babelnet.io", "YourAPIKeyHere")
 	version := client.GetVersion()
-	fmt.Println(version)
+	assert.Equal(t, version, "V4_0")
+}
+
+func TestClient_GetSynSetIds(t *testing.T) {
 	ids := client.GetSynSetIds("apple", "EN")
-	fmt.Println(ids[0].ID)
+	assert.Equal(t, len(ids), 17)
+	assert.Equal(t, ids[0].ID, "bn:00289737n")
+}
+
+func TestClient_GetSynSet(t *testing.T) {
 	synSet := client.GetSynSet("bn:14792761n")
-	fmt.Println(synSet.Categories)
+	assert.Equal(t, len(synSet.Categories), 4)
+}
+
+func TestClient_GetSenses(t *testing.T) {
 	req := new(BabelSenseRequest)
 	req.SetLemma("apple").SetSearchLang("EN")
 	result := client.GetSenses(req)
-	fmt.Println(result[0].Type)
+	assert.Equal(t, len(result), 261)
+	assert.Equal(t, result[0].Type, "WordNetSense")
+}
 
-	req2 := new(BabelIdRequest)
-	req2.SetId("trousers").SetSearchLang("EN").SetPos("NOUN").SetSource("WIKI")
-	response := client.GetBabelNetId(req2)
-	fmt.Println(response)
+func TestClient_GetBabelNetIds(t *testing.T) {
+	req := new(BabelIdRequest)
+	req.SetId("trousers").SetSearchLang("EN").SetPos("NOUN").SetSource("WIKI")
+	response := client.GetBabelNetId(req)
+	assert.Equal(t, len(response), 2)
+	assert.Equal(t, response[0].ID, "bn:00060423n")
+}
 
-	response2 := client.GetOutgoingEdges("bn:14792761n")
-	fmt.Println(response2)
+func TestClient_GetOutgoingEdges(t *testing.T) {
+	response := client.GetOutgoingEdges("bn:14792761n")
+	assert.Equal(t, len(response), 561)
+	assert.Equal(t,response[0].Language, "MUL")
 }
